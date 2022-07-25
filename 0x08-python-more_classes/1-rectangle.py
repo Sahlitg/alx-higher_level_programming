27be310fa113fe4b481b486728997e07c64348c5
sahlitg <sahlit2006@gmail.com>
2022-07-25 09:05:05 -1200

    tsk 1 fix

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,48 +1,36 @@
 #!/usr/bin/python3
-"""
-A rectangle with width and height.
-"""
+""" Rectangle class """
 
 
 class Rectangle:
-    """
-    Rectangle functions and data
-    """
+    """ class Rectangle that defines a rectangle """
+    __height = None
+    __width = None
 
-    def __init__(self, width=0, height=0):
-        """ Instantiation
-        """
-        self.width = width
-        self.height = height
-
-    @property
-    def width(self):
-        """ Getter for width
-        """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ Setter for width
-        """
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+    def __init__(self, prmWidth=0, prmHeight=0):
+        self.height = prmHeight
+        self.width = prmWidth
 
     @property
     def height(self):
-        """ Getter for height
-        """
         return self.__height
 
     @height.setter
-    def height(self, value):
-        """ Setter for height
-        """
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
+    def height(self, prmValue):
+        if not isinstance(prmValue, int):
+            raise TypeError("height must be an integer")
+        if prmValue < 0:
             raise ValueError("height must be >= 0")
-        self.__height = value
+        self.__height = prmValue
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, prmValue):
+        if not isinstance(prmValue, int):
+            raise TypeError("width must be an integer")
+        if prmValue < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = prmValue
